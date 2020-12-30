f29ea3b1203ff620832292f4666be7d3ba67a0a9
yerlansarsenov <sarsenov.erlan.1999@gmai.com>
2020-12-30 13:46:07 +0600

    intentFor<>

--- Lab1/app/src/main/java/kz/moviedb/lab1/lesson2_sandbox/TestActivity.kt
+++
@@ -1,7 +0,0 @@
-package kz.moviedb.lab1.lesson2_sandbox
-
-/**
- * Created by Sarsenov Yerlan on 18.12.2020.
- */
-class TestActivity {
-}
---
+++ Lab1/app/src/main/java/kz/moviedb/lab1/lesson2_sandbox/TestActivity2.kt
@@ -0,0 +1,11 @@
+package kz.moviedb.lab1.lesson2_sandbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test2)
+    }
+}
